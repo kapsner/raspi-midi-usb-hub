167652e21f57236cc5c215413cb41624f93082a1
kapsner <lorenz.kapsner@web.de>
2024-03-23 08:27:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install byobu git ruby wget
+sudo apt-get install -y byobu git ruby wget
 
 sudo wget -O /usr/local/bin/connectall.rb https://gist.githubusercontent.com/chmanie/4f2838f4548d25b9c883f7d6d074f67c/raw/5d73927cf8f8bc1055963559aa44e7faa00926ed/connectall.rb
 sudo chmod +x /usr/local/bin/connectall.rb
